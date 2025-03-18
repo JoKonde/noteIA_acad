f0e6f384907b6f7a65d5a6a40d9bfcdcda5a99d8
Jonathan Konde <jonathankonde430@gmail.com>
2025-03-18 10:31:44 +0100

    first design

---
+++ NoteIA/landing/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'landing/index.html')
+
+def login(request):
+    return render(request, 'landing/login.html')
+
+def signup(request):
+    return render(request, 'landing/signup.html')
